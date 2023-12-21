2af93ca544b8e4f15ba7af4bb87da3d86909bae4
TinNguyen <ductin16111@gmail.com>
2023-12-21 11:32:32 +0200

    event listern with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,58 @@ import PopUp from './components/PopUp'
 import Word from './components/Word'
 import WrongLetters from './components/WrongLetters'
 import Notification from './components/Notification'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const words = ['react', 'tranquil', 'mechanization', 'christmas']
+const selectedWord = words[Math.floor(Math.random() * words.length)]
+
 
 function App() {
-    const words = ['react', 'tranquil', 'mechanization', 'christmas']
-    const selectedWord = words[Math.floor(Math.random() * words.length)]
-
     const [playable, setPlayable] = useState(true)
     const [correctLetters, setCorrectLetters] = useState([])
     const [wrongLetters, setWrongLetters] = useState([])
+
+    useEffect(() => {
+        const handleKeydown = event => {
+            const { key, keyCode } = event
+            if (playable && keyCode >= 65 && keyCode <= 90) {
+                const letter = key.toLowerCase()
+                if (selectedWord.includes(letter)) {
+                    if (!correctLetters.includes(letter)) {
+                        setCorrectLetters(currentLetters => [...currentLetters, letter])
+                    }
+                    else {
+                        // show notification for correct letters
+                    }
+                }
+                else {
+                    if (!wrongLetters.includes(letter)) {
+                        setWrongLetters(currentLetters => [...currentLetters, letter])
+                    }
+                    else {
+                        // show notification for wrong letters
+                    }
+                }
+            }
+        }
+        // listen for the event 'press key down' from window
+        // then func handleKeydown is called
+        window.addEventListener('keydown', handleKeydown)
+
+        // clean up all the event listeners created
+        // so we only have 1 EL at 1 time
+        return () => window.removeEventListener('keydown', handleKeydown)
+    }, [correctLetters, wrongLetters, playable])
 
     return (
         <>
             <Header />
             <div className='game-ctn'>
                 <Figure />
-                <WrongLetters wrongLetters={wrongLetters}/>
-                <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
+                <WrongLetters wrongLetters={wrongLetters} />
+                <Word selectedWord={selectedWord} correctLetters={correctLetters} />
                 <PopUp />
-                <Notification wrongLetters={wrongLetters}/>
+                <Notification wrongLetters={wrongLetters} />
             </div>
         </>
     )
