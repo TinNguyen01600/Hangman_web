ce9cf2f8d949183df474c885f07719e007cc32ca
TinNguyen <ductin16111@gmail.com>
2023-12-21 12:36:58 +0200

    update feature show notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,23 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 const Notification = ({ wrongLetters, enteredLetter, setEnteredLetter }) => {
     return (
         <div>
-            {/* Popup */}
             <Popup
                 open={wrongLetters.some(letter => letter === enteredLetter)}
                 modal
                 nested
+                onClose={() => setEnteredLetter('')}    // called whenever popup box close, regardless of whether it was closed by clicking the close button inside the popup or clicking outside the popup.
             >
                 {close => (
                     <div className='popup-ctn'>
                         You have already entered this letter
-                        <button onClick={() => {
-                            close()
-                            setEnteredLetter('')
-                        }}>Close</button>
+                        <button onClick={close}>Close</button>
                     </div>
                 )}
             </Popup>
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -2,19 +2,26 @@ import Popup from "reactjs-popup"
 import 'reactjs-popup/dist/index'
 
 const PopUp = () => {
+    const renderPopupContent = (close) => (
+        <div>
+            <p>This is the popup content.</p>
+            <button onClick={close}>Close Popup</button>
+        </div>
+    );
+
     return (
         <div>
             <Popup
                 trigger={<button>Click to open pop-up</button>}
                 modal
                 nested
+                onClose={() => console.log(123)}
             >
-                {close => (
-                    <div className='popup-ctn'>
-                        Message
-                        <button onClick={() => close()}>Play again</button>
-                    </div>
-                )}
+                {/* {(close) => renderPopupContent(close)} */}
+                <div>
+                    <p>This is the popup content.</p>
+                    <button onClick={close}>Close Popup</button>
+                </div>
             </Popup>
         </div>
     )
